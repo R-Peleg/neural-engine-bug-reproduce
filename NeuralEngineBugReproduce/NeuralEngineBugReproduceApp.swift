e059c35963dbcc4c0a180e898a5fb9dee56accc4
Reuven Peleg <reuven.peleg@orcam.com>
2022-11-28 11:18:13 +0200

    Initial Commit

---
+++ NeuralEngineBugReproduce/NeuralEngineBugReproduceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeuralEngineBugReproduceApp.swift
+//  NeuralEngineBugReproduce
+//
+//  Created by Reuven Aviad Peleg on 28/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct NeuralEngineBugReproduceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
